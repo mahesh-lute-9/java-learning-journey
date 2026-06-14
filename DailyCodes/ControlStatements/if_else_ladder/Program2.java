7ed89b04536d099b5aaf4207a0ddc4dd38ba28bd
Mahesh <maheshlute2005@gmail.com>
2026-06-14 13:29:40 +0000

    understand if else ladder statements

---
+++ DailyCodes/ControlStatements/if_else_ladder/Program2.java
@@ -0,0 +1,31 @@
+
+
+class Demo{
+
+	public static void main(String[] args){
+
+		int x = 118;
+
+		int y = 19;
+
+		int z = 17;
+
+		if(x > y && x > z){
+
+			System.out.println(x + " is greater");
+		
+		}else if(y > z){
+		
+			System.out.println(y + " is greater");
+		}else{
+			
+			System.out.println(z + " is greater");
+		}
+	}
+}
+
+
+/* Output:
+ * 	118 is greater
+ *
+ * */
